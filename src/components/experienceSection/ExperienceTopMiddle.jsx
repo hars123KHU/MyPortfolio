596e56beb6d177e76a334f8dbeea5ebdc7dafe41
Harshita Kushwaha <166895018+hars123KHU@users.noreply.github.com>
2025-06-28 21:02:47 +0530

    Update ExperienceTopMiddle.jsx

--- src/components/experienceSection/ExperienceTopMiddle.jsx
+++ src/components/experienceSection/ExperienceTopMiddle.jsx
@@ -4,7 +4,7 @@ const ExperienceTopMiddle = () => {
   return (
     <div className="lg:w-[35%] md:w-[50%] sm:w-[80%]">
       <img
-        src="../../public/images/experience-image.png"
+        src="../../images/experience-image.png"
         alt="experience-image"
       />
     </div>
